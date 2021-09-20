54dd8d742f16827a7d52595413c3e088eaf47137
Pedro Sottili <pedro.sottili@gmail.com>
2021-09-20 18:19:23 -0300

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ axios.defaults.baseURL = 'http://localhost:8080';
 
 function AbrirModal() {
   let modal = document.querySelector(".modal");
-  // document.getElementById("editID").setAttribute("value", id);
   modal.style.display = "block";
 }
 
@@ -21,16 +20,21 @@ async function cadastrar(event){
     const confirmasenha=document.getElementById("confirmasenha");
 
     if(senha.value==confirmasenha.value){
-        alert('Cadastro realizado com sucesso!');
-        const user={
-            nome:nome.value,
-            cpf:cpf.value,
-            email:email.value,
-            senha:senha.value
-        };
-        const {data} = await axios.post('/formulario', user);
+      const user={
+        nome:nome.value,
+        cpf:cpf.value,
+        email:email.value,
+        senha:senha.value
+      };
+      const {data} = await axios.post('/formulario', user);
+      alert('Cadastro realizado com sucesso!');
 
-        console.log(data);
+      console.log(data);
     }
 
+}
+
+function NovaAba(url) {
+  const win = window.open(url, "_blank");
+  win.focus();
 }
